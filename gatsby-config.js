5b047e1d9aafe3bd2661d4ca6f4f9f253754f0f7
Eric Pierpont <epierpont@gmail.com>
2021-09-03 12:15:23 -0600

    Created posts and blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,19 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://erixusedbikepartsmain.gatsbyjs.io",
     title: "Erix Used Bikes Parts",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
   ],
 };
