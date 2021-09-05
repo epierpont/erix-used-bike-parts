002f1f88592fd5554acf3c39d39cedb0cab02d20
Eric Pierpont <epierpont@gmail.com>
2021-09-05 08:27:08 -0600

    Now using featured images

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,31 +1,41 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 
 const BlogPost = ({ data }) => {
-    return (
-        <Layout pageTitle={data.mdx.frontmatter.title}>
-            <p>{data.mdx.frontmatter.date}</p>
-            <MDXRenderer>
-                {data.mdx.body}
-            </MDXRenderer>
-        </Layout>
-    )
+  const image = getImage(data.mdx.frontmatter.hero_image)
+
+  return (
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image}
+        alt={data.mdx.frontmatter.hero_image_alt} />
+      <MDXRenderer>
+        {data.mdx.body}
+      </MDXRenderer>
+    </Layout>
+  )
 }
 
-// highlight-start
+
 export const query = graphql`
-  query ($id: String) {
-    mdx(id: {eq: $id}) {
+  query($slug: String) {
+    mdx(slug: {eq: $slug}) {
+      body
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
-      body
     }
   }
 `
-// highlight-end
 
 export default BlogPost
